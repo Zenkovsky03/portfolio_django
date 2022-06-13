b683518e0ecbd68523d885206fae9363e3391b1c
Zenkovsky03 <mateuszzaniewski1@gmail.com>
2022-06-13 23:18:48 +0200

    add templates

--- base/models.py
+++ base/models.py
@@ -17,7 +17,7 @@ class Project(models.Model):
 
 class Skill(models.Model):
   title = models.CharField(max_length=200)
-  body = models.TextField(null=True)
+  body = models.TextField(null=True, blank=True)
   created = models.DateTimeField(auto_now_add=True)
   id = models.UUIDField(default=uuid.uuid4, unique=True, primary_key=True, editable=True)
 
--- base/views.py
+++ base/views.py
@@ -1,7 +1,17 @@
+from email.quoprimime import body_check
+from multiprocessing import context
 from django.shortcuts import render
-from .models import Project
+from .models import Project, Skill
 # Create your views here.
 
 def homePage(request):
   projects = Project.objects.all()
-  return render(request, 'base/home.html', {'projects':projects})
+  skills = Skill.objects.filter(body = '') #zawiera pusty opis
+  detailedSkills = Skill.objects.exclude(body = '') #wyklucza pusty opis
+  context = {'projects':projects, 'skills':skills, 'detailedSkills': detailedSkills}
+  return render(request, 'base/home.html', context)
+
+def projectPage(request, pk):
+  project = Project.objects.get(id=pk)
+  context = {'project':project}
+  return render(request, 'base/project.html', context)
